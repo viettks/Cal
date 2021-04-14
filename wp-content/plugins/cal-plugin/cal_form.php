9154836f83737754485564740c09a3b42bd29965
viettks <viettks@gmail.com>
2021-04-14 17:15:32 +0700

    update

--- wp-content/plugins/cal-plugin/cal_form.php
+++ wp-content/plugins/cal-plugin/cal_form.php
@@ -1,30 +1,69 @@
-<div class="col-lg-5 col-sm-12 col-12">
-          
-        <div class="card">
+       <style>
+           .card{
+               background-color: #ffffff;
+               margin: 20px;
+               padding: 50px;
+               border-radius: 20px;
+           }
+           .card-header{
+               margin: 0px;
+               padding: 10px;
+               background-color: #00000008;
+           }
+           .cal-label{
+                color: #212529 ;
+                flex: 0 0 33.333333%;
+                max-width: 33.333333%;
+           }
+           .flex-row{
+                margin-bottom: 1rem;
+                display: flex;
+                flex-wrap: wrap;
+                
+           }
+           .size-row{
+                flex: 0 0 33.333333%;
+                max-width: 33.333333%;
+                padding-top: 10px;
+
+            }
+            .cal-right{
+                flex: 0 0 66.666666%;
+                max-width: 66.666666%;
+                padding-top: 10px;
+            }
+            .form-input{
+                width: 100%;
+            }
+
+
+       </style>
+       
+       <div class="card">
             <div class="card-header">
-                <h5 class="card-title" style="padding:0px;margin:0px;">Tính giá tem nhãn tự động</h5>
+                <h4 class="card-title">Tính giá tem nhãn tự động</h4>
             </div>
             <div class="card-body">
                 <form id="calculate_form" name="caculator_form">
                     <div class="flex-row">
-                        <label class="left-col">Kích thước (Đơn vị mm)</label>
-                        <div>
-                            <input type="number" placeholder="dài (mm)" name="width" id="cal_width">
+                        <label class="cal-label">Kích thước (Đơn vị mm)</label>
+                        <div class="size-row">
+                            <input type="number" class="form-input" placeholder="dài (mm)" name="width" id="cal_width">
                         </div>
-                        <div>
-                            <input type="number" placeholder="Rộng (mm)" name="cal_height">    
-                        </div>
-                    </div>
-                    <div class="flex-row">
-                        <label class="left-col">Số lượng cần in</label>
-                        <div class="right-col">
-                            <input type="number" placeholder="Nhập số lượng nhãn in" name="amount" id="cal_amout">
+                        <div class="size-row">
+                            <input type="number"  class="form-input" placeholder="Rộng (mm)" name="cal_height" id="cal_height">    
                         </div>
                     </div>
                     <div class="flex-row">
-                        <label class="left-col">Công nghệ in</label>
-                        <div class="right-col">
-                            <select name="print_type" id="print_type">
+                        <label class="cal-label">Số lượng cần in</label>
+                        <div class="cal-right">
+                            <input type="number" class="form-input" placeholder="Nhập số lượng nhãn in" name="amount" id="cal_amout">
+                        </div>
+                    </div>
+                    <div class="flex-row">
+                        <label class="cal-label">Công nghệ in</label>
+                        <div class="cal-right">
+                            <select name="print_type" id="print_type" class="form-input">
                                 <?php foreach ($print_techs as $pr) : ?>
                                     <option value="<?php echo $pr->print_id ?>"><?php echo $pr->print_name ?> </option>
                                 <?php endforeach?>
@@ -33,9 +72,9 @@
                         </div>
                     </div>
                     <div class="flex-row">
-                        <label class="left-col">Chất liệu</label>
-                        <div class="right-col">
-                            <select name="material_type" id="material_type">
+                        <label class="cal-label">Chất liệu</label>
+                        <div class="cal-right">
+                            <select name="material_type" id="material_type" class="form-input">
                                 <?php foreach ($materials as $material) : ?>
                                     <option value="<?php echo $material->material_id ?>"><?php echo $material->material_name ?> </option>
                                 <?php endforeach?>
@@ -43,9 +82,9 @@
                         </div>
                     </div>
                     <div class="flex-row">
-                        <label class="left-col">Gia công</label>
-                        <div class="right-col">
-                            <select id="menbran_type" name="menbran_type">
+                        <label class="cal-label">Gia công</label>
+                        <div class="cal-right">
+                            <select id="menbran_type" name="menbran_type" class="form-input">
                             <?php foreach ($membrans as $membran) : ?>
                                 <option value="<?php echo $membran->membran_id ?>"><?php echo $membran->membran_name; ?> </option> -->
                             <?php endforeach?>
@@ -55,5 +94,110 @@
                 </form>
             </div>
         </div>
-
     </div>
+    <div id="temporary_wrap" style="display: none;">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title">Giá tạm tính</h5>
+            </div>
+            <div class="card-body">
+                <form id="temporary" name="temporary_form">
+                    <div class="flex-row">
+                        <label class="cal-label">Kích thước</label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_size">
+                            <label id="temporary_size_label">1</label>
+                        </div>
+                        
+                    </div>
+                    <div class="flex-row">
+                        <label class="cal-label">Số lượng</label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_amout">
+                            <label id="temporary_amout_label">1</label>
+                        </div>
+                        
+                    </div>
+                    <div class="flex-row">
+                        <label class="cal-label">Loại decan</label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_material">
+                            <label id="temporary_material_label">1</label>
+                        </div>
+                        
+                    </div>
+                    <div class="flex-row">
+                        <label class="cal-label">Đơn giá </label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_price">
+                            <label id="temporary_price_label">1</label>
+                        </div>
+                       
+                    </div>
+                    <div class="flex-row">
+                        <label class="cal-label">Thành tiền </label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_total">
+                            <label id="temporary_total_label">1</label>
+                        </div>
+                        
+                    </div>
+                    <div class="flex-row">
+                        <label  class="cal-label">VAT</label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_vat">
+                            <label id="temporary_vat_label">1</label>
+                        </div>
+                    </div>
+                    <div class="flex-row">
+                        <label class="cal-label">Tổng tiền</label>
+                        <div class="cal-right">
+                            <input type="hidden" id="temporary_total_vat">
+                            <label id="temporary_total_vat_label">1</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script
+    src="https://code.jquery.com/jquery-3.6.0.js"
+    integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+    crossorigin="anonymous"></script>
+    <script>
+    $("#cal_width, #cal_height,#cal_amout,#print_type,#material_type,#menbran_type").change(function () {
+        
+        if($("#cal_width").val()!=""&&$("#cal_height").val()!=""&&$("#cal_amout").val()!=""){
+            var data = {
+					action : "get_ajax_price",
+                    width : $("#cal_width").val(),
+                    height : $("#cal_height").val(),
+                    amount : $("#cal_amout").val(),
+                    print_tech_id : $("#print_type").val(),
+                    material_id : $("#material_type").val(),
+                    membran_id : $("#menbran_type").val(),
+				};
+            $.ajax({
+			    type: "post",
+				url: "<?php echo admin_url( 'admin-ajax.php' ); ?>?"+ $.param(data),
+                dataType : "json", 
+                context: this,
+				success: function(data)
+				{
+                        $('#temporary_size_label').text(data.size);
+                        $('#temporary_amout_label').text(data.amount);
+                        $('#temporary_material_label').text(data.decal_type);
+                        $('#temporary_price_label').text(data.price + " VND/m2");
+                        $('#temporary_total_label').text(data.total_price);
+                        $('#temporary_vat_label').text(data.vat_price);
+                        $('#temporary_total_vat_label').text(data.total_price_vat);
+                        $('#temporary_wrap').show();
+				}
+				,error: function(xhr) {
+				}
+			});
+        }else{
+            
+        }
+    })
+    </script>
--- wp-content/plugins/cal-plugin/cal_plugin.php
+++ wp-content/plugins/cal-plugin/cal_plugin.php
@@ -12,7 +12,10 @@
 ?>
 <?php
 if(!class_exists('CaculatorForm')){
+
+
     class CaculatorForm{
+
         function get_form() {
             $print_techs = $this->get_print_tech();
             $materials = $this->get_material();
@@ -100,6 +103,8 @@ if(!class_exists('CaculatorForm')){
         
     }
 }
+
+
 function caculatorFormLoading(){
     global $cal_form ;
     $cal_form = new CaculatorForm();
@@ -141,7 +146,15 @@ function caculatorFormLoading(){
     COLLATE= utf8mb4_general_ci
     ENGINE=InnoDB";
     dbDelta($sql);
+    if(!function_exists('add_shortcode')) {
+        return;
+    }
+    add_shortcode( 'caculator_form' , "getShortCodeCaculatorForm");
 
+}
+function getShortCodeCaculatorForm(){
+    global $cal_form ;
+    return $cal_form->get_form();
 }
 function disableCaculatorForm(){
     global $wpdb;
@@ -153,20 +166,13 @@ function disableCaculatorForm(){
 add_action( 'plugins_loaded', 'caculatorFormLoading' );
 register_deactivation_hook( __FILE__, 'disableCaculatorForm' );
 
-function enqueue_scripts_and_styles()
-{
-        wp_enqueue_script('jquery');
-        wp_register_script('my-plugin-script', plugins_url( '/js/script.js', __FILE__ ));
-        wp_enqueue_script( 'my-plugin-script' );
- 
-}
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts_and_styles' );
+// add_action('wp_enqueue_scripts', 'callback_for_setting_up_scripts');
+// function callback_for_setting_up_scripts() {
+//     wp_register_style( 'caculatorStyle', get_style_uri(). '/lib/caculator-form.css', array(), get_theme_version() );
+//     wp_enqueue_style( 'caculatorStyle' );
+// }
 
 ?>
-
-
-
-
 <?php
  
 function cal_form_create_menu() {
@@ -304,57 +310,72 @@ function caculatorPrice(){
         wp_send_json($result, 200 );
 
     }else{
-        wp_send_json(null, 400 );
+        wp_send_json(null, 404 );
     }
 
     die();
 }
-add_action ('wp_ajax_nopriv_my_action', 'caculatorPrice');
+add_action( 'wp_ajax_get_ajax_price', 'caculatorPrice' );
+add_action( 'wp_ajax_nopriv_get_ajax_price', 'caculatorPrice' ); 
 
 
 
 function setting_pages() {
+    
     $cal_form = new CaculatorForm();
     $materials = $cal_form->get_material();
     $membrans = $cal_form->get_membran();
     $print_techs = $cal_form->get_print_tech();
     $i = 1;
+    echo 'short_code : [caculator_form]'
     
 ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <div class="wrap">
 <h1>Cài đặt form</h1>
-<?php echo admin_url( 'admin-post.php' ) ?>
-<h2>Cài đặt chất liệu</h2>
-<table id="tb_material">
+
+<div>
+<h2 class="card-title">Cài đặt chất liệu</h2>
+<div class="card-body">
+<table id="tb_material" class="table table-striped">
+<thead>
 <tr>
     <th>STT</th>
     <th>Tên chất liệu</th>
     <th>action</th>
 </tr>
+</thead>
+<tbody >
 <?php foreach($materials as $material) : ?>
     <tr>
-        <td><?php echo $i++?></td>
+        <td scope="row"><?php echo $i++?></td>
         <td><?php echo $material->material_name;?></td>
-        <td><button onclick="del_material(<?php echo $material->material_id;?>)" >Xóa</button></td>
+        <td><button class="btn btn-danger"  onclick="del_material(<?php echo $material->material_id;?>)" >Xóa</button></td>
     </tr>
 
 <?php endforeach ?>
-
+</tbody>
 </table>
-<form onsubmit="return false;"> 
-    <label for="add_material_name">Tên chất liệu</label>
-    <input type="text" name="material_name" id="add_material_name">
-    <button id="btn_add_material" type="button" >Thêm chất liệu</button>
+<form onsubmit="return false;" class="form-inline">
+    <div class="form-group mx-sm-3 mb-2">
+        <label for="add_material_name">Tên chất liệu : </label>
+        <input type="text" name="material_name" id="add_material_name">
+    </div>
+    <button id="btn_add_material" type="button" class="btn btn-primary mb-2" >Thêm chất liệu</button>
+    
  </form>
-
-
+</div>
+</div>
 <h2>Cài đặt màng bọc</h2>
-<table>
+<table class="table table-striped">
+    <thead>
 <tr>
     <th>STT</th>
     <th>Tên màng</th>
     <th>giá tiền/m2</th>
 </tr>
+    </thead>
+    <tbody>
 <?php 
     $i = 1;
     foreach($membrans as $membran) : ?>
@@ -362,92 +383,216 @@ function setting_pages() {
         <td><?php echo $i++?></td>
         <td><?php echo $membran->membran_name;?></td>
         <td><?php echo $membran->membran_price;?></td>
-        <td><button onclick="del_membran(<?php echo $membran->membran_id;?>)" >Xóa</button></td>
+        <td><button class="btn btn-danger" onclick="del_membran(<?php echo $membran->membran_id;?>)" >Xóa</button></td>
     </tr>
 
 <?php endforeach ?>
+    </tbody>
 
 </table>
-<form onsubmit="return false;"> 
-    <label for="add_membran_name">Tên màng</label>
-    <input type="text" name="membran_name" id="add_membran_name">
-    <label for="add_membran_price">Giá tiền/m2</label>
-    <input type="number" name="membran_price" id="add_membran_price">
-    <button id="btn_add_membran" type="button" >Thêm màng</button>
- </form>
+<form onsubmit="return false;" style="max-width: 700px;">
+    <div class="form-group row">
+        <h3>Thêm màng </h3>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_name" class="col-sm-2 col-form-label">Tên màng</label>
+      <div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="Tên màng" name="membran_name" id="add_membran_name">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-2 col-form-label">Giá tiền/m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền" name="membran_price" id="add_membran_price">
+      </div>
+    </div>
+   
+    <div class="form-group row">
+      <div class="col-sm-10">
+        <button id="btn_add_membran" class="btn btn-primary" type="button" >Thêm màng</button>
+      </div>
+    </div>
+  </form>
 
 <h2>Cài đặt công nghệ in</h2>
-<table>
+<table class="table table-striped">
+    <thead>
 <tr>
 
-    <th rowspan="2">STT</th>
-    <th rowspan="2">Tên công nghệ</th>
-    <th colspan="6">giá tiền/m2</th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th></th>
-    <th rowspan="2">action</th>
+    <th rowspan="2" class="text-center" >STT</th>
+    <th rowspan="2" class="text-center">Tên công nghệ</th>
+    <th colspan="6" class="text-center">giá tiền/m2</th>
+    <th rowspan="2" class="text-center">action</th>
 </tr>
 <tr>
-    <th></th>
-    <th></th>
-    <th>Dưới 10m2</th>
-    <th>10-30m2</th>
-    <th>30-50m2</th>
-    <th>50-100m2</th>
-    <th>100-200m2</th>
-    <th>trên 200m2</th>
-    <th></th>
+    <th class="text-center">Dưới 10m2</th>
+    <th class="text-center">10-30m2</th>
+    <th class="text-center">30-50m2</th>
+    <th class="text-center">50-100m2</th>
+    <th class="text-center">100-200m2</th>
+    <th class="text-center"> trên 200m2</th>
 </tr>
+    </thead>
+    <tbody>
 <?php 
     $i = 1;
-    echo admin_url( 'admin-ajax.php' );
+    
     foreach($print_techs as $pr) : ?>
     <tr>
-        <td><?php echo $i++?><input type="hidden" name="id" id="id" value="<?php echo $pr->print_id?>"></td>
-        <td><?php echo $pr->print_name;?></td>
-        <td><?php echo $pr->print_price;?></td>
-        <td><?php echo $pr->print_price_30;?></td>
-        <td><?php echo $pr->print_price_50;?></td>
-        <td><?php echo $pr->print_price_100;?></td>
-        <td><?php echo $pr->print_price_200;?></td>
-        <td><?php echo $pr->print_price_big;?></td>
-        <td><button onclick="edit_to_frm(this)" >Chỉnh sửa</button><button onclick="del_print_tech(<?php echo $pr->print_id;?>)" >Xóa</button></td>
+        <td class="text-center"><?php echo $i++?><input type="hidden" name="id" id="id" value="<?php echo $pr->print_id?>"></td>
+        <td class="text-center"><?php echo $pr->print_name;?></td>
+        <td class="text-center"><?php echo $pr->print_price;?></td>
+        <td class="text-center"><?php echo $pr->print_price_30;?></td>
+        <td class="text-center"><?php echo $pr->print_price_50;?></td>
+        <td class="text-center"><?php echo $pr->print_price_100;?></td>
+        <td class="text-center"><?php echo $pr->print_price_200;?></td>
+        <td class="text-center"><?php echo $pr->print_price_big;?></td>
+        <td class="text-center"><button class="btn btn-primary" onclick="edit_to_frm(this)" >Chỉnh sửa</button><button class="btn btn-danger" onclick="del_print_tech(<?php echo $pr->print_id;?>)" >Xóa</button></td>
     </tr>
 
 <?php endforeach ?>
+    </tbody>
 </table>
 
-<form onsubmit="return false;"> 
-    <label for="add_membran_name">Tên màng</label>
-    <input type="hidden" name="print_id" id="print_id">
-    <input type="text" name="print_name" id="print_name">
-    <label for="add_membran_price">Dưới 10</label>
-    <input type="number" name="print_price" id="print_price">
-    <label for="add_membran_price">Dưới 2</label>
-    <input type="number" name="print_price_30" id="print_price_30">
-    <label for="add_membran_price">Dưới 3</label>
-    <input type="number" name="print_price_50" id="print_price_50">
-    <label for="add_membran_price">Dưới 4</label>
-    <input type="number" name="print_price_100" id="print_price_100">
-    <label for="add_membran_price">Dưới 5</label>
-    <input type="number" name="print_price_200" id="print_price_200">
-    <label for="add_membran_price">Dưới 6</label>
-    <input type="number" name="print_price_big" id="print_price_big">
-  
-    <button id="btn_save_print" type="button" >Lưu công nghệ in</button>
- </form>
-
-
+<form onsubmit="return false;" style="max-width: 700px;">
+    <div class="form-group row">
+        <h3>Thêm công nghệ in </h3>
+    </div>
+    <div class="form-group row">
+        <label for="print_name" class="col-sm-3 col-form-label">Tên công nghệ in</label>
+      <div class="col-sm-10">
+        <input type="hidden" name="print_id" id="print_id">
+        <input type="text" class="form-control" placeholder="Tên công nghệ in" name="print_name" id="print_name">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-3 col-form-label">Dưới 10m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền dưới 10m2" name="print_price" id="print_price">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-3 col-form-label">10-30m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền từ 10m2 - 30m2" name="print_price_30" id="print_price_30">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-3 col-form-label">30-50m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền từ 30m2 - 50m2" name="print_price_50" id="print_price_50">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-3 col-form-label">50-100m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền từ 50m2 - 100m2" name="print_price_100" id="print_price_100">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-3 col-form-label">100-200m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền từ 100m2 - 200m2" name="print_price_200" id="print_price_200">
+      </div>
+    </div>
+    <div class="form-group row">
+        <label for="add_membran_price" class="col-sm-3 col-form-label">trên 200m2</label>
+        <div class="col-sm-10">
+        <input type="number" class="form-control"  placeholder="Giá tiền trên 200m2" name="print_price_big" id="print_price_big">
+      </div>
+    </div>
+   
+    <div class="form-group row">
+      <div class="col-sm-10">
+        <button id="btn_save_print"  class="btn btn-primary"  type="button" >Lưu công nghệ in</button>
+      </div>
+    </div>
+  </form>
 </div>
 <script
   src="https://code.jquery.com/jquery-3.6.0.js"
   integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
   crossorigin="anonymous"></script>
+<script>
+    $( document ).ready(function() {
+    
+    $('#btn_add_material').click(function(){
+        save_material();
+    });
+    $('#btn_add_membran').click(function(){
+        save_membran();
+    });
+    $('#btn_save_print').click(function(){
+        save_print_tech();
+    })
+});
+
+function save_material(){
+    var data = {
+        "action":"save_material",
+        "material_name": $('#add_material_name').val(),
+    };
+    window.location.href = "<?php echo admin_url( 'admin-post.php' ); ?>?" + $.param(data);
+}
+function del_material(id){
+    var data = {
+        "action":"detete_material",
+        "material_id": id,
+    };
+    window.location.href = "<?php echo admin_url( 'admin-post.php' ); ?>?" + $.param(data);
+}
+
+function save_membran(){
+    var data = {
+        "action":"save_membran",
+        "membran_name": $('#add_membran_name').val(),
+        "membran_price": $('#add_membran_price').val(),
+    };
+    window.location.href = "<?php echo admin_url( 'admin-post.php' ); ?>?" + $.param(data);
+}
+function del_membran(id){
+    var data = {
+        "action":"delete_membran",
+        "membran_id": id,
+    };
+    window.location.href = "<?php echo admin_url( 'admin-post.php' ); ?>?" + $.param(data);
+}
+
+function save_print_tech(){
+    var data = {
+        "action":"save_print_tech",
+        "print_id":$('#print_id').val(),
+        "print_name":$('#print_name').val(),
+        "print_price":$('#print_price').val(),
+        "print_price_30":$('#print_price_30').val(),
+        "print_price_50":$('#print_price_50').val(),
+        "print_price_100":$('#print_price_100').val(),
+        "print_price_200":$('#print_price_200').val(),
+        "print_price_big":$('#print_price_big').val(),
+    };
+    window.location.href = "<?php echo admin_url( 'admin-post.php' ); ?>?" + $.param(data);
+}
+function del_print_tech(id){
+    var data = {
+        "action":"delete_print_tech",
+        "print_id": id,
+    };
+    window.location.href = "<?php echo admin_url( 'admin-post.php' ); ?>?" + $.param(data);
+}
+function edit_to_frm(e){
+    var tbr = $(e.closest('tr'));
+    $('#print_id').val(tbr.find('td input').val());
+    $('#print_name').val(tbr.find('td')[1].innerText);
+    $('#print_price').val(tbr.find('td')[2].innerText);
+    $('#print_price_30').val(tbr.find('td')[3].innerText);
+    $('#print_price_50').val(tbr.find('td')[4].innerText);
+    $('#print_price_100').val(tbr.find('td')[5].innerText);
+    $('#print_price_200').val(tbr.find('td')[6].innerText);
+    $('#print_price_big').val(tbr.find('td')[7].innerText);
+}
+</script>
 <?php
-wp_enqueue_script( 'cal-js', plugins_url( 'js\script.js', __FILE__ ));
+//wp_enqueue_script( 'cal-js', plugins_url( 'js\script.js', __FILE__ ));
 } ?>
 
 
